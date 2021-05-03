e031a28e78426353cb592a6438f260a009e7e8c7
Pranshu Agrawal <43396684+IMPranshu@users.noreply.github.com>
2021-05-03 16:50:35 +0530

    Add files via upload

--- powerof2.cpp
+++ powerof2.cpp
@@ -1,15 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool isPowerof2(int n){
-	return (n && !(n & n-1));
+
+int setBit(int n , int pos){
+	return (n | 1<<pos);
+}
+bool getBit(int n,int pos){
+	return ((n & (1<<pos))!=0);
+}
+int unique(int arr[], int n){
+		int result=0;
+	for(int i=0;i<64;i++){
+		int sum =0;
+		for(int j=0;j<n;j++){
+			int a =getBit(arr[j],i);
+			if(a){
+				sum++;
+			}
+		}
+		if(sum % 3 != 0){
+			result = setBit(result , i);
+		}
+	}
+	return result;
 }
 int main()
 {
-	if(isPowerof2(0))
-	cout<<"\nPower of 2";
-	else
-	cout<<"\nNot power of 2";
+	int a[]={1,3,2,3,4,2,1,1,3,2};
+	cout<<unique(a,10);
 
 	
 	return 0;
