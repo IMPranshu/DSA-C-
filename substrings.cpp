f12f4e3213462f8a612c5cc63cad00e8d2eeabcb
Pranshu Agrawal <43396684+IMPranshu@users.noreply.github.com>
2021-05-04 20:35:25 +0530

    Add files via upload

---
+++ substrings.cpp
@@ -0,0 +1,22 @@
+//Substrings
+
+#include<bits/stdc++.h>
+using namespace std;
+void subseq(string s,string ans){
+	if(s.length()==0){
+		cout<<ans<<endl;
+		return ;
+	}
+	
+	
+	char ch = s[0];
+	string ros = s.substr(1);
+	
+	subseq(ros, ans);
+	subseq(ros, ans+ch);
+}
+
+int main(){
+	subseq("ABCD","");
+	return 0;
+}
