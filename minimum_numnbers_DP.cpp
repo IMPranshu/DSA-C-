f9bfbaee276d0b3413ef28a6df066ef5eceae5f8
P K Agrawal <pranshukumar1999@gmail.com>
2021-08-06 19:26:35 +0530

    Tabulation Implemented

--- minimum_numnbers_DP.cpp
+++ minimum_numnbers_DP.cpp
@@ -2,6 +2,8 @@
 #include <string>
 using namespace std;
 
+#define vi vector<int>
+
 const int N = 1e5 + 2, MOD = 1e9 + 7;
 int dp[N];
 
@@ -22,12 +24,24 @@ int MinSquare(int n)
 
 int main()
 {
-    for (int i = 0; i < N; i++)
-    {
-        dp[i] = MOD;
-    }
+
     int n;
     cin >> n;
-    cout << MinSquare(n) << endl;
+    vi dp(n + 1, MOD);
+
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 3;
+    for (int i = 1; i * 1 <= n; i++)
+    {
+        for (int j = 0; i * i + j <= n; j++)
+        {
+            dp[i * i + j] = min(dp[i * i + j], 1 + dp[j]);
+        }
+    }
+
+    cout << dp[n] << endl;
+
     return 0;
 }
