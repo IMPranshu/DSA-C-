982a0237c4b2f3c47e3a7fcc6f1f42be08f116f3
P K Agrawal <pranshukumar1999@gmail.com>
2021-08-06 13:45:20 +0530

    Qestion Solved

--- infosys1.cpp
+++ infosys1.cpp
@@ -2,6 +2,10 @@
 #include <string>
 using namespace std;
 
+int P[100], B[100];
+void Merge(int A[], int C[], int low, int high, int mid);
+void MergeSort(int A[], int B[], int low, int high);
+
 int main()
 {
     int n, e, defeat = 0;
@@ -14,15 +18,25 @@ int main()
     cout << "\nEnter all Bonus";
     for (int i = 0; i < n; i++)
         cin >> B[i];
+
+    int PointB[1000];
+    for (int i = 0; i < n; i++)
+    {
+        B[P[i]] = B[i];
+    }
+
+    sort(P, P + n);
+
     for (int i = 0; i < n; i++)
     {
         if (P[i] <= e)
         {
             defeat++;
-            e += B[n];
+            e += B[P[i]];
         }
         else
             i = n;
     }
+
     cout << "\nMonsters defeated:" << defeat;
 }
