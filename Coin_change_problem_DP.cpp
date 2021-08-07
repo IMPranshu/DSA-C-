7e76e6aae7ed4a15944cd85e50ea702d2c8b92be
P K Agrawal <pranshukumar1999@gmail.com>
2021-08-07 11:49:47 +0530

    Tabulation Implemented

--- Coin_change_problem_DP.cpp
+++ Coin_change_problem_DP.cpp
@@ -3,32 +3,12 @@
 using namespace std;
 
 #define vi vector<int>
+#define vvi vector<vi>
 
 const int N = 1e3 + 2, MOD = 1e9 + 7;
-int dp[N][N];
 
-int CoinChange(vi &a, int n, int x)
-{
-    if (x == 0)
-        return 1;
-    if (x < 0)
-        return 0;
-    if (n <= 0)
-        return 0;
-
-    if (dp[n][x] != -1)
-        return dp[n][x];
-
-    dp[n][x] = CoinChange(a, n, x - a[n - 1]) + CoinChange(a, n - 1, x);
-    return dp[n][x];
-}
 int main()
 {
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-            dp[i][j] = -1;
-    }
 
     int n;
     cin >> n;
@@ -40,7 +20,22 @@ int main()
     }
     int x;
     cin >> x;
-    cout << CoinChange(a, n, x);
 
+    vvi dp(n + 1, vi(x + 1, 0));
+    dp[0][0] = 1;
+
+    for (int i = 1; i < n + 1; i++)
+    {
+        for (int j = 0; j < x + 1; j++)
+        {
+            if (j - a[i - 1] >= 0)
+            {
+                dp[i][j] += dp[i][j - a[i - 1]];
+            }
+            dp[i][j] += dp[i - 1][j];
+        }
+    }
+
+    cout << dp[x];
     return 0;
 }
