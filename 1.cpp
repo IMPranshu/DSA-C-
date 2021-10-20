b884af1adaa35835d5d60ab87ed2aea20fd40896
Pranshu Agrawal <43396684+IMPranshu@users.noreply.github.com>
2021-10-20 10:17:50 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int i;
+	cin>>i;
+	for(int j=1;j<=i;j++)
+		{
+		for(int k=1;k<=j;k++)
+			cout<<"*";
+		cout<<endl;
+	}
+	return 0;
+}
